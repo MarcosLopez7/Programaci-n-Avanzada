5d5bd0aec1ce861167656a6ca54b0a2cc725b0c1
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-04-10 19:39:49 -0500

    completed

--- Tareas/Fiolosofa/main.c
+++ Tareas/Fiolosofa/main.c
@@ -1,43 +1,90 @@
+#include <pthread.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include "sem.h"
+#include <time.h>
+#include <unistd.h>
 
-int sems;
+#define NF 5
 
-void ciclo(int id) {
-	srand(getpid());
+pthread_mutex_t palillos_mutex[NF];
 
-	do {
-            printf("F %d: pensando...\n", id);
-	    sleep(rand()%5);
-
-	    Wait(sems, id);
-	    Wait(sems, (id+1)%5);
-
-            printf("F %d: comiendo (palillos %d y %d).\n", id, id, (id+1)%5);
-	    sleep(rand()%5);
-
-	    Signal(sems, (id+1)%5);
-	    Signal(sems, id);
-	} while(1);
-}
+void *filosofo(void *);
+int palillos[NF];
 
 int main() {
-        int i;
-	sems = Getsem(5);
 
-	for(i=0;i<5;i++) {
-		Signal(sems, i);
-	}
+  int i;
 
-	for(i=0;i<5;i++) {
-		if(!fork()) ciclo(i);
-	}
-	for(i=0;i<5;i++) {
-		wait(0);
-	}
+  srand((int)time(NULL));
 
-	exit(0);
+  for (i = 0; i < NF; ++i) {
+    palillos[i] = 1;
+  }
+
+  pthread_t filosofar[NF];
+
+  for (i = 0; i < NF; ++i) {
+    int *arg = malloc(sizeof(*arg) * 2);
+
+    if (i == 0) {
+      *arg = 5;
+    } else {
+      *arg = i - 1;
+    }
+    arg++;
+    *arg = i;
+    arg--;
+
+    pthread_create(&filosofar[i], NULL, filosofo, (void *)arg);
+  }
+
+  for (i = 0; i < NF; ++i) {
+    pthread_join(filosofar[i], NULL);
+  }
+
+  return 0;
+}
+
+void *filosofo(void *p) {
+  int *palos = (int *)p;
+
+  while (1) {
+
+    pthread_mutex_lock(&palillos_mutex[*palos]);
+    pthread_mutex_lock(&palillos_mutex[*(palos + 1)]);
+
+    if (palillos[*palos] && palillos[*(palos + 1)]) {
+      palillos[*palos] = 0;
+      palillos[*(palos + 1)] = 0;
+
+      pthread_mutex_unlock(&palillos_mutex[*palos]);
+      pthread_mutex_unlock(&palillos_mutex[*(palos + 1)]);
+
+      printf("Filosofo %d: voy a comer\n", *(palos + 1));
+      sleep(5);
+
+      pthread_mutex_lock(&palillos_mutex[*palos]);
+      pthread_mutex_lock(&palillos_mutex[*(palos + 1)]);
+
+      palillos[*palos] = 1;
+      palillos[*(palos + 1)] = 1;
+
+      pthread_mutex_unlock(&palillos_mutex[*palos]);
+      pthread_mutex_unlock(&palillos_mutex[*(palos + 1)]);
+
+      printf("Filosofo %d: voy a pensar\n", *(palos + 1));
+
+      sleep((rand() % 10) + 10);
+
+      printf("Filosofo %d: voy a comer, necesito, pensar\n", *(palos + 1));
+    } else {
+      pthread_mutex_unlock(&palillos_mutex[*palos]);
+      pthread_mutex_unlock(&palillos_mutex[*(palos + 1)]);
+      sleep(1);
+    }
+  }
+
+  free(p);
+  pthread_exit(NULL);
 }
